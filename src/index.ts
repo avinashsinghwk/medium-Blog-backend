72a98761b064b0e8e565c1a5b8c9a6bd0cc40494
Avinash Kumar Singh <abhinashsinghwk2@gmail.com>
2025-01-24 17:51:49 +0530

    add origin in cors

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://medium-blog-frontend.vercel.app'
+}))
 
 app.route('/api/v1/user', userRoutes)
 
